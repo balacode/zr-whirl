f01fd6b4c8e884bd4854910fc7b16a570cb704bd
balarabe@protonmail.com <balarabe@protonmail.com>
2018-06-14 18:12:36 +0100

    Format file headers

--- module.go
+++ module.go
@@ -1,6 +1,6 @@
 // -----------------------------------------------------------------------------
 // (c) balarabe@protonmail.com                                      License: MIT
-// :v: 2018-05-24 04:12:11 2B12AC                           [zr-whirl/module.go]
+// :v: 2018-05-24 04:12:11 DB0447                           zr-whirl/[module.go]
 // -----------------------------------------------------------------------------
 
 // Package whirl implements the Whirlpool hashing algorithm.
